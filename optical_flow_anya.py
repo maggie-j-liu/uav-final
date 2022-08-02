6d5ed00cf1caa0692aace03977646a6a16e9783f
Anya <anyakhosla05@gmail.com>
2022-08-02 12:39:53 -0700

    optical flow

--- optical_flow_anya.py
+++ optical_flow_anya.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from djitellopy import Tello
+import time
 
 
 class TelloOpticalFlow:
@@ -9,9 +10,11 @@ class TelloOpticalFlow:
         self.drone.connect()
         self.drone.streamon()
 
-        self.frame = None
+        print(self.drone.get_battery())
 
-        self.px_movements = np.array()
+        self.frame = None
+        self.px_movements = []
+        self.time0 = time.time()
 
     def get_frame(self):
         try:
@@ -22,27 +25,42 @@ class TelloOpticalFlow:
             print(e)
 
     def sparse_optical_flow_lk(self):
-        feature_params = dict(maxCorners =  15, qualityLevel = 0.01, minDistance = 2, blockSize = 7) # helps decide what corners should be tracked
+        feature_params = dict(maxCorners =  15, qualityLevel = 0.01, minDistance = 2, blockSize = 7) 
         frame0 = self.get_frame()
         frame0_gray = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-        corners0 = cv2.goodFeaturesToTrack(frame0_gray, mask=None, **feature_params) # add mask from led detection so that we have a ROI
+        corners0 = cv2.goodFeaturesToTrack(frame0_gray, mask=None, **feature_params)
 
         lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-        while True:
+        while time.time() - self.time0 < 150:
             self.get_frame()
+            cv2.imshow("Frame", self.frame)
+            cv2.waitKey(1)
+
             frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
             corners1, state, errors = cv2.calcOpticalFlowPyrLK(frame0_gray, frame_gray, corners0, None, **lk_params)
 
-            found_current = corners1[state == 1]
-            found_prev = corners0[state == 1]
+            if corners1 is not None:
+                print('corners found')
+                found_current = corners1[state == 1]
+                found_prev = corners0[state == 1]
 
-            if found_current.shape == found_prev.shape:
                 changes = found_current - found_prev
-                avg_change = np.sum(changes, axis=0) / changes.shape[0] # assuming ROI
-                np.append(self.px_movements, avg_change)
+                if changes.shape[0] != 0:
+                    avg_change = np.sum(changes, axis=0) / changes.shape[0]
+                    self.px_movements.append(avg_change)
+
+                frame0_gray = frame_gray.copy()
+                corners0 = np.reshape(found_current, (-1, 1, 2))
+            else:
+                print('none found')
+
+        cv2.destroyAllWindows()
 
 
+if __name__ == "__main__":
+    driver = TelloOpticalFlow()
+    driver.sparse_optical_flow_lk()
 
 
 
