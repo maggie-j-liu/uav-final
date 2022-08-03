b68cc46e68e5018ce082a5980c61a297bb12666c
Anya <anyakhosla05@gmail.com>
2022-08-03 12:25:46 -0700

    added optical flow updates

--- optical_flow_merged.py
+++ optical_flow_merged.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 from djitellopy import Tello
-import time
 
 
 class TelloOpticalFlow:
@@ -12,7 +11,6 @@ class TelloOpticalFlow:
 
         self.frame = None
         self.px_movements = []
-        self.time0 = time.time()
 
         self.PX_TO_MM_FACTOR = 0.0264583    # may be inaccurate
         self.feature_params = dict(maxCorners =  15, qualityLevel = 0.01, minDistance = 2, blockSize = 7)
@@ -29,37 +27,45 @@ class TelloOpticalFlow:
     def sparse_optical_flow_lk(self, led_mask):
         frame0 = self.get_frame()
         frame0_gray = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-        corners0 = cv2.goodFeaturesToTrack(frame0_gray, led_mask, **self.feature_params)
+        corners0 = cv2.goodFeaturesToTrack(frame0_gray, mask=led_mask, **self.feature_params)
 
-        while time.time() - self.time0 < 1.5:
+        while True:
             self.get_frame()
             cv2.imshow("Frame", self.frame)
-            cv2.waitKey(1)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
 
             frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
             corners1, state, errors = cv2.calcOpticalFlowPyrLK(frame0_gray, frame_gray, corners0, None, **self.lk_params)
 
-            if corners1 is not None:
-                print('corners found')
-                found_current = corners1[state == 1]
-                found_prev = corners0[state == 1]
+            if corners1 is None:
+                corners0 = cv2.goodFeaturesToTrack(frame0_gray, mask=led_mask, **self.feature_params)
+                corners1, state, errors = cv2.calcOpticalFlowPyrLK(frame0_gray, frame_gray, corners0, None, **self.lk_params)
 
-                changes = found_current - found_prev
-                if changes.shape[0] != 0:
-                    avg_change = np.sum(changes, axis=0) / changes.shape[0]
-                    
-                    curr_len = np.mean([found_current[1][0]-found_current[0][0], found_current[2][0]-found_current[3][0], 
-                                    found_current[3][1]-found_current[0][1], found_current[2][1]-found_current[1][1]])
-                    prev_len = np.mean([found_current[1][0]-found_current[0][0], found_current[2][0]-found_current[3][0], 
-                                    found_current[3][1]-found_current[0][1], found_current[2][1]-found_current[1][1]])
-                    fb_change = (curr_len - prev_len) / self.PX_TO_MM_FACTOR
-                    np.insert(avg_change, 1, fb_change)
-                    
-                    self.px_movements.append(avg_change)
 
-                frame0_gray = frame_gray.copy()
-                corners0 = np.reshape(found_current, (-1, 1, 2))
-            else:
-                print('no corners found')
+            found_current = corners1[state == 1]
+            found_prev = corners0[state == 1]
+
+            changes = found_current - found_prev
+            if changes.shape[0] != 0:
+                avg_change = np.sum(changes, axis=0) / changes.shape[0]
+                
+                curr_len = np.mean([found_current[1][0]-found_current[0][0], found_current[2][0]-found_current[3][0], 
+                                found_current[3][1]-found_current[0][1], found_current[2][1]-found_current[1][1]])
+                prev_len = np.mean([found_current[1][0]-found_current[0][0], found_current[2][0]-found_current[3][0], 
+                                found_current[3][1]-found_current[0][1], found_current[2][1]-found_current[1][1]])
+                fb_change = (curr_len - prev_len) / self.PX_TO_MM_FACTOR
+                np.insert(avg_change, 1, fb_change)
+                
+                self.px_movements.append(avg_change)
+
+            frame0_gray = frame_gray.copy()
+            corners0 = np.reshape(found_current, (-1, 1, 2))
         
         return self.px_movements
+
+
+if __name__ == "__main__":
+    driver = TelloOpticalFlow()
+    driver.sparse_optical_flow_lk(None)
