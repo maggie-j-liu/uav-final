4f5cc324c269e4af1b0ddc36d23d2b91e9a48153
Maggie Liu <maggie.j.liu@gmail.com>
2022-08-03 12:44:09 -0700

    start integration of detection + optical flow

--- main.py
+++ main.py
@@ -7,35 +7,24 @@ from process_image import process_image
 from optical_flow_merged import TelloOpticalFlow
 from light_tracking_pid import track_light
 
-tello = Tello()
-tello.connect()
-tello.streamon()
-frame_read = tello.get_frame_read()
-
+tello_optical_flow = TelloOpticalFlow()
+tello = tello_optical_flow.tello
 battery = tello.get_battery()
 print(f"battery: {battery}")
 
 while True:
-    # LED detection
-    img = frame_read.frame
-    img = process_image(img)
-    cv.imshow("camera", img)
-    led_mask = None # ADD LED DETECTION RESULT
-
     # Optical flow
-    tello_optical_flow = TelloOpticalFlow()
-    tello_optical_flow.get_frame()
-    px_movements = tello_optical_flow.sparse_optical_flow_lk(led_mask)
+    px_movements = tello_optical_flow.sparse_optical_flow_lk()
 
-    #PID control 
-    for m in len(px_movements):
-        track_light(px_movements[m])
+    # PID control
+    for movement in px_movements:
+        track_light(movement, tello)
 
-    # WaitKey 
+    # WaitKey
     key = cv.waitKey(1) & 0xFF
     if key == ord("q"):
         break
-    time.sleep(1 / 5)
+    time.sleep(1 / 3)
 
 tello.streamoff()
 tello.land()
--- optical_flow_merged.py
+++ optical_flow_merged.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from djitellopy import Tello
 import time
+from led_detection import detect_leds
 
 
 class TelloOpticalFlow:
@@ -9,35 +10,50 @@ class TelloOpticalFlow:
         self.tello = Tello()
         self.tello.connect()
         self.tello.streamon()
+        self.tello.takeoff()
+        self.frame_read = self.tello.get_frame_read()
 
         self.frame = None
         self.px_movements = []
         self.time0 = time.time()
 
         self.PX_TO_MM_FACTOR = 0.0264583    # may be inaccurate
-        self.feature_params = dict(maxCorners =  15, qualityLevel = 0.01, minDistance = 2, blockSize = 7)
-        self.lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        self.feature_params = dict(
+            maxCorners=15, qualityLevel=0.01, minDistance=2, blockSize=7)
+        self.lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(
+            cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
     def get_frame(self):
         try:
-            self.frame = self.tello.get_frame_read().frame
-            self.frame = np.array(self.frame, dtype='uint8')
+            self.frame = self.frame_read.frame
             return self.frame
         except Exception as e:
             print(e)
 
-    def sparse_optical_flow_lk(self, led_mask):
+    def sparse_optical_flow_lk(self):
         frame0 = self.get_frame()
+
+        led_mask = detect_leds(np.copy(self.frame))
+        # convert img_mask, which is a numpy array, to an array where the value is 0 if the pixel is black and 1 if the pixel is white
+        led_mask = led_mask[:, :, 0]
+        led_mask = led_mask // 255
         frame0_gray = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-        corners0 = cv2.goodFeaturesToTrack(frame0_gray, led_mask, **self.feature_params)
+        corners0 = cv2.goodFeaturesToTrack(
+            frame0_gray, mask=led_mask, **self.feature_params)
 
         while time.time() - self.time0 < 1.5:
             self.get_frame()
             cv2.imshow("Frame", self.frame)
             cv2.waitKey(1)
 
+            led_mask = detect_leds(np.copy(self.frame))
+            # convert img_mask, which is a numpy array, to an array where the value is 0 if the pixel is black and 1 if the pixel is white
+            led_mask = led_mask[:, :, 0]
+            led_mask = led_mask / 255
+
             frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-            corners1, state, errors = cv2.calcOpticalFlowPyrLK(frame0_gray, frame_gray, corners0, None, **self.lk_params)
+            corners1, state, errors = cv2.calcOpticalFlowPyrLK(
+                frame0_gray, frame_gray, corners0, None, **self.lk_params)
 
             if corners1 is not None:
                 print('corners found')
@@ -47,19 +63,19 @@ class TelloOpticalFlow:
                 changes = found_current - found_prev
                 if changes.shape[0] != 0:
                     avg_change = np.sum(changes, axis=0) / changes.shape[0]
-                    
-                    curr_len = np.mean([found_current[1][0]-found_current[0][0], found_current[2][0]-found_current[3][0], 
-                                    found_current[3][1]-found_current[0][1], found_current[2][1]-found_current[1][1]])
-                    prev_len = np.mean([found_current[1][0]-found_current[0][0], found_current[2][0]-found_current[3][0], 
-                                    found_current[3][1]-found_current[0][1], found_current[2][1]-found_current[1][1]])
+
+                    curr_len = np.mean([found_current[1][0]-found_current[0][0], found_current[2][0]-found_current[3][0],
+                                        found_current[3][1]-found_current[0][1], found_current[2][1]-found_current[1][1]])
+                    prev_len = np.mean([found_current[1][0]-found_current[0][0], found_current[2][0]-found_current[3][0],
+                                        found_current[3][1]-found_current[0][1], found_current[2][1]-found_current[1][1]])
                     fb_change = (curr_len - prev_len) / self.PX_TO_MM_FACTOR
                     np.insert(avg_change, 1, fb_change)
-                    
+
                     self.px_movements.append(avg_change)
 
                 frame0_gray = frame_gray.copy()
                 corners0 = np.reshape(found_current, (-1, 1, 2))
             else:
                 print('no corners found')
-        
+
         return self.px_movements
