9fccd182e550b10cf67c851d703cf3e9ba68b190
Anya <anyakhosla05@gmail.com>
2022-08-05 12:40:27 -0700

    audio

--- audio_detection.py
+++ audio_detection.py
@@ -1,32 +1,10 @@
-import speech_recognition
+from vosk_api.python.example.test_microphone import Microphone
 
 class AudioDetection:
     def __init__(self):
-        self.tracking_color = None
-        self.recognizer = speech_recognition.Recognizer()
-
-    def detect_audio(self):
-        text = None
-
-        with speech_recognition.Microphone() as source:
-            audio = self.recognizer.listen(source)
-
-        try:
-            text = self.recognizer.recognize_sphinx(audio)
-            print(text)
-        except speech_recognition.UnknownValueError:
-            print('audio could not be determined')
-        except speech_recognition.RequestError as e:
-            print(e)
-
-        if text is not None:
-            print(text)
-            words = text.split()
-            index = words.rfind('track')
-            if index != -1:
-                self.tracking_color = words[index + 1]
-
+        mic = Microphone()
+        mic.get_text()
+        
 if __name__ == '__main__':
     driver = AudioDetection()
-    driver.detect_audio()
 
